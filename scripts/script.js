3d755de6d40b40adc73235257d1ca58a1e5d16e0
Andrey Lementa <andrejkudrasov79070@gmail.com>
2023-05-18 18:32:18 +0300

    Anc task

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+let firstName = prompt("Whats your name");
+console.log(firstName);
+
+let lastName = prompt("Whats your lastname");
+console.log(lastName);
+
+let age = prompt("How old are you?");
+console.log(age);
